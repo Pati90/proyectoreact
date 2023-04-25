a8778863d93c39bedff505db30b2ee9640c1de3c
Pablo Ferreccio <ferrecciop@gmail.com>
2023-04-25 17:36:03 -0300

    Componentes pedidos hechos y funcionando

---
+++ pre-entrega-1/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div>
+            <i class="bi bi-cart"></i>
+        </div>
+    );
+};
+
+export default CartWidget;
+
--- pre-entrega-1/src/components/NavBar/NavBar.jsx
+++ pre-entrega-1/src/components/NavBar/NavBar.jsx
@@ -2,4 +2,15 @@
 // NavBar componente contenedor y las categorias los componentes presentacionales.
 //"hardcodear" código es escribir código que deberia ser automatizado o código que debería ser obtenido por el usuario final y no por el desarrollador.
 
-export const NavBar = React.createClass({
+import CartWidget from "../CartWidget/CartWidget";
+
+const NavBar = () => {
+    return (
+        <div style={{width: "100%", heigth: "100%"}}>
+            <h1>Giladitas 3D </h1>
+            <CartWidget/>
+        </div>
+    );
+};
+
+export default NavBar; 
