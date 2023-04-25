3409d7cd442f8f7b526ebbbfb15653724090f4d7
Pablo Ferreccio <ferrecciop@gmail.com>
2023-04-25 16:05:04 -0300

    Estructura inicial de la entrega

---
+++ pre-entrega-1/src/components/NavBar.js
@@ -0,0 +1,5 @@
+// ultimo commit debe decir PreEntrega1Ferreccio. NavBar debe incluir: titulo (giladitas3d), categorias clickeables, bootstrap, 
+// NavBar componente contenedor y las categorias los componentes presentacionales.
+//"hardcodear" código es escribir código que deberia ser automatizado o código que debería ser obtenido por el usuario final y no por el desarrollador.
+
+export const NavBar = React.createClass({
