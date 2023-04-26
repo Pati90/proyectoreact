06ae399514974cb78ebbe95f4a7574895b7eb0f5
Pablo Ferreccio <ferrecciop@gmail.com>
2023-04-26 09:20:47 -0300

    Pre-Entrega-1-Ferreccio

--- pre-entrega-1/src/App.js
+++ pre-entrega-1/src/App.js
@@ -1,5 +1,5 @@
 import NavBar from "./components/NavBar/NavBar";
-
+import  'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- pre-entrega-1/src/components/NavBar/NavBar.jsx
+++ pre-entrega-1/src/components/NavBar/NavBar.jsx
@@ -3,12 +3,36 @@
 //"hardcodear" código es escribir código que deberia ser automatizado o código que debería ser obtenido por el usuario final y no por el desarrollador.
 
 import CartWidget from "../CartWidget/CartWidget";
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div style={{width: "100%", heigth: "100%"}}>
+        <div style={{margin: "1cm", width: "100%", heigth: "100%"}}>
             <h1>Giladitas 3D </h1>
-            <CartWidget/>
+          
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        
+  <a class="navbar-brand" href="#"><CartWidget/></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Acerca de</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Servicios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+    </ul>
+  </div>
+</nav>
         </div>
     );
 };
